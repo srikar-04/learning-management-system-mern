3dd4b3698ce63ad1ce6ccb4d2990e4e005db4f3b
srikar <srikar.code01@gmail.com>
2025-02-06 15:44:24 +0530

    add tabs component and implement tab navigation in AuthPage

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,12 +1,13 @@
 import { createContext } from "react";
 
-
+// AuthContext, which is created using createContext, acts as a global variable from which all components can acess the data without passing props to each and every component
 export const AuthContext  = createContext(null)
 
 export default function AuthProvider({children}) {
     return (
         <AuthContext.Provider value={{}}>
-            {children}
+            {/* this children is "app" */}
+            {children} 
         </AuthContext.Provider>
     )
 }
--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../components/ui/tabs.jsx'
+import { GraduationCap } from 'lucide-react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function AuthPage() {
+    
+    const [activeTab, setActiveTab] = useState('signin')
+
+    const handleTabChange = (value) => {
+        setActiveTab(value)
+    }
+
   return (
-    <div>AuthPage</div>
+    <div className='flex flex-col h-screen w-full'>
+        <header className='px-4 lg:px-6 h-14 flex items-center border-b'>
+            <Link className='flex items-center justify-center' >
+                <GraduationCap className='h-8 w-8 mr-4' />
+                <span className='font-extrabold text-xl'>LMS Learn</span>
+            </Link>
+        </header>
+        <div className='flex items-center justify-center min-h-screen bg-background'>
+            <Tabs
+                value={activeTab}
+                defaultValue='signin'
+                onValueChange={handleTabChange}
+                className='w-full max-w-md'
+            >
+                <TabsList className='w-full border grid grid-cols-2'>
+                    <TabsTrigger value='signin'>Sign In</TabsTrigger>
+                    <TabsTrigger value='signup'>Sign Up</TabsTrigger>
+
+                    <TabsContent value='signin' >signin</TabsContent>
+                    <TabsContent value='signup' >signup</TabsContent>
+                </TabsList>
+            </Tabs>
+        </div>
+    </div>
   )
 }
 
