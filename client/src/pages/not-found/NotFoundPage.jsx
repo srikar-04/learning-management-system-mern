d9ce7bc99b411cf1bbda9d5e973d1c23e3348584
srikar <srikar.code01@gmail.com>
2025-02-08 10:28:11 +0530

    feat: add NotFoundPage component for handling 404 errors

---
+++ client/src/pages/not-found/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFoundPage() {
+  return (
+    <div className='w-full h-screen text-3xl flex justify-center items-center bg-zinc-800 text-white'>
+        <div className='font-serif'>Page Not Found 404</div>
+    </div>
+  )
+}
+
+export default NotFoundPage
