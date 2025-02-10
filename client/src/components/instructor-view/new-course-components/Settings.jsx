655976c882bad64ae73452555ee03d312a271180
srikar <srikar.code01@gmail.com>
2025-02-10 23:50:28 +0530

    feat: add multi-step loader for image upload in Settings component and update loading state management

--- client/src/components/instructor-view/new-course-components/Settings.jsx
+++ client/src/components/instructor-view/new-course-components/Settings.jsx
@@ -1,40 +1,56 @@
-import { Label } from '../../../components/ui/label.jsx'
-import { Card, CardContent, CardHeader, CardTitle } from '../../../components/ui/card.jsx'
-import React, { useContext } from 'react'
-import { Input } from '../../../components/ui/input.jsx'
-import { InstructorContext } from '@/context/instructor-context/instructorContext.jsx'
-import { mediaUploadService } from '../../../services/services.js'
+import { Label } from "../../../components/ui/label.jsx";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../../../components/ui/card.jsx";
+import React, { useContext, useState } from "react";
+import { Input } from "../../../components/ui/input.jsx";
+import { InstructorContext } from "@/context/instructor-context/instructorContext.jsx";
+import { mediaUploadService } from "../../../services/services.js";
+import { MultiStepLoader } from "@/components/ui/multi-step-loader.jsx";
 
 function Settings() {
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
 
-  const {
-    courseLandingFormData,
-    setCourseLandingFormData
-  } = useContext(InstructorContext)
+  const [loading, setLoading] = useState(false);
+
+  const loadingStates = [
+    {
+      text: "uploading file",
+    },
+    {
+      text: "fetching details",
+    },
+  ];
 
   const handleImageUploadChange = async (e) => {
-    const selectedImage = e.target.files[0]
+    setLoading(true);
+    const selectedImage = e.target.files[0];
 
-    if(selectedImage) {
+    if (selectedImage) {
       const imageFormData = new FormData();
-      imageFormData.append('file', selectedImage)
+      imageFormData.append("file", selectedImage);
       try {
-        const response = await mediaUploadService(imageFormData)
+        const response = await mediaUploadService(imageFormData);
         console.log(response);
 
-        if(response.success) {
+        if (response.success) {
           setCourseLandingFormData({
             ...courseLandingFormData,
-            image: response.data.url
-          })
+            image: response.data.url,
+          });
+          setLoading(false)
         }
       } catch (error) {
-        console.log('error while uploading image, in settings.jsx file', error);
+        console.log("error while uploading image, in settings.jsx file", error);
+        setLoading(false);
       }
     }
-  }
+  };
   console.log(courseLandingFormData);
-  
 
   return (
     <Card>
@@ -42,23 +58,33 @@ function Settings() {
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
       <CardContent>
-        {
-          courseLandingFormData?.image 
-          ? <img src={courseLandingFormData.image} /> 
-          : (
-            <div className='flex flex-col gap-3'>
+        {loading ? (
+          <MultiStepLoader
+            loadingStates={loadingStates}
+            loading={loading}
+            duration={2000}
+          />
+        ) : courseLandingFormData?.image ? (
+          <img
+            src={courseLandingFormData.image}
+            alt="Course Image"
+            // onLoad tells whether the image is completely loaded or not
+            onLoad={() => setLoading(false)}
+            className="w-full h-auto object-cover"
+          />
+        ) : (
+          <div className="flex flex-col gap-3">
             <Label>Upload Course Image</Label>
-            <Input 
-              type='file'
-              accept='image/*'
-              onChange = {(e) => handleImageUploadChange(e)}
+            <Input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUploadChange}
             />
-           </div>
-          )
-        }
+          </div>
+        )}
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export default Settings
+export default Settings;
