28add3a97b557f5a519712d8cc363f46bc1b291a
srikar <srikar.code01@gmail.com>
2025-02-13 18:09:29 +0530

    feat: add Course model and routes for course management

---
+++ server/src/models/course.models.js
@@ -0,0 +1,36 @@
+import mongoose, {Schema} from "mongoose";
+
+const lectureSchema = new Schema({
+    title: String,
+    freePreview: String,
+    videoUrl: String,
+    public_id: Boolean
+})
+
+const CourseSchema = new Schema({
+    instructorId: String,
+    instructorName: String,
+    date: Date,
+    title: String,
+    category: String,
+    level: String,
+    primaryLanguage: String,
+    subtitle: String,
+    description: String,
+    pricing: Number,
+    objectives: String,
+    welcomeMessage: String,
+    image: String,
+    students: [
+        {
+            studentId : String,
+            studentName: String,
+            studentEmail: String
+        }
+    ],
+    curriculum: [lectureSchema]
+}, {timestamps: true})
+
+const Course = mongoose.model("Course", CourseSchema)
+
+export { Course }
