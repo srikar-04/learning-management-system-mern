0ee045a96487ff23f474b5966b81ba3cb5e31861
srikar <srikar.code01@gmail.com>
2025-02-17 03:26:26 +0530

    feat: implement student course list display and fetching on StudentCoursesPage

--- client/src/pages/student/StudentCoursesPage.jsx
+++ client/src/pages/student/StudentCoursesPage.jsx
@@ -8,20 +8,48 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { ArrowUpDownIcon } from "lucide-react";
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { filterOptions, sortOptions } from "../../config/index.js";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import { fetchStudentCourseListService } from "@/services/services.js";
+import { studentContext } from "@/context/student-context/studentContext.jsx";
+import { Card, CardContent, CardTitle } from "@/components/ui/card.jsx";
 
 function StudentCoursesPage() {
   const [sort, setSort] = useState("");
 
+  const {
+    studentCourseList,
+    setStudentCourseList
+  } = useContext(studentContext)
+
+  const fetchAllStudentCouses = async () => {
+    const response = await fetchStudentCourseListService();
+
+    if(!response?.success) {
+      throw new Error('error fetching student courses')
+    }
+
+    setStudentCourseList(response.data)
+
+    // console.log(response.data, 'student courses in useEffect');
+  }
+
+  useEffect( () => {
+    fetchAllStudentCouses()
+  }, [])
+
+  const handleFilterOnChange = () => {
+
+  }
+
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-6 p-4">
       <h1 className="text-3xl font-bold mb-4">All Courses</h1>
       <div className="flex flex-col md:flex-row gap-4">
         <aside className="w-full md:w-64 space-y-4">
-          <div className="p-4 space-y-4">
+          <div className="space-y-4">
             {Object.keys(filterOptions).map((keyItem) => (
               <div className="p-4 space-y-4">
                 <h3 className="font-bold mb-3">{keyItem.toUpperCase()}</h3>
@@ -73,7 +101,48 @@ function StudentCoursesPage() {
                 </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
+            <span className="text-sm text-gray-800 font-bold">10 Results</span>
           </div>
+
+          <div className="space-y-4">
+              {
+                studentCourseList && studentCourseList.length > 0
+                ? studentCourseList.map(courseItem => (
+                  <Card className='cursor-pointer' key={courseItem?._id}>
+
+                    <CardContent className='flex gap-4 p-4'>
+
+                      <div className="w-48 h-32 flex-shrink-0">
+                        <img className="w-full h-full object-cover" src={courseItem?.image} alt="course thumbnail" />
+                      </div>
+
+                      <div className="flex-1 ">
+                        <CardTitle className='text-xl mb-2' >
+                          {courseItem?.title}
+                        </CardTitle>
+                        <p className="text-sm text-gray-600 mb-1">
+                          created by   <span className="font-bold text-gray-700">
+                            {courseItem?.instructorName}
+                          </span>
+                        </p>
+                        <p className="text-[17px] text-gray-600 mt-3 mb-2">
+                          {
+                            `${courseItem?.curriculum?.length} ${courseItem?.curriculum?.length <=1 ? 'Lecture' : 'Lectures'} - ${courseItem?.level?.toUpperCase()} Level`
+                          }
+                        </p>
+                        <p className="font-bold text-lg">
+                          ${courseItem?.pricing}
+                        </p>
+                      </div>
+
+                    </CardContent>
+
+                  </Card>
+                ))
+                : <h1>No Courses Found</h1>
+              }
+          </div>
+
         </main>
       </div>
     </div>
